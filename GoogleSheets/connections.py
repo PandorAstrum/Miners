f7b52970053b2596491304447844f443d4ac5890
PandorAstrum <primeintegerslab@gmail.com>
2018-04-14 17:41:34 +0600

    git ignore update

---
+++ GoogleSheets/connections.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+__author__ = "Ashiquzzaman Khan"
+__desc__ = "Google Sheet Connection files"
+"""
+
+# connections
+
+
+# duplicate check
+
+# update
+
+# insert
+
+# apply styles
